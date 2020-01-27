b6ad7e959f7de1be7ab81f95d71e8ffd732423ef
Mitty <mitty.fun@gmail.com>
2020-01-27 23:39:13 +0800

    init

---
+++ ai.js
@@ -0,0 +1,44 @@
+
+
+function AI(game) {
+
+    const { width, height, grid, turn } = game
+    const arr = []
+
+    function getScoreOf(x, y, turn) {
+        grid[x][y].color = turn
+        const a = getLine(x, y, 1, 0) + getLine(x, y, -1, 0) + 1;
+        const b = getLine(x, y, 0, 1) + getLine(x, y, 0, -1) + 1;
+        const c = getLine(x, y, 1, 1) + getLine(x, y, -1, -1) + 1;
+        const d = getLine(x, y, 1, -1) + getLine(x, y, -1, 1) + 1;
+        grid[x][y].color = ''
+
+        return [a, b, c, d].reduce((sum, length) => {
+            return sum + 10 ** length
+        }, 0)
+    }
+
+    function getLine(x, y, vx, vy) {
+        let i
+        for (i = 1; i < 5; i++) {
+            if (grid[x][y].color !== grid[x + vx * i][y + vy * i].color) break
+        }
+        return i - 1;
+    }
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            if (grid[x][y].color === '') {
+                const attack = getScoreOf(x, y, turn)
+                const defend = getScoreOf(x, y, turn === 'b' ? 'w' : 'b')
+                const score = attack * 1.1 + defend
+                grid[x][y].score = score
+                arr.push({ x, y, score })
+            }
+        }
+    }
+
+    const best = arr.sort((a, b) => a.score - b.score).pop()
+
+    game.place(best.x, best.y)
+}
---
+++ gomoku.js
@@ -0,0 +1,50 @@
+class Gomoku {
+
+    constructor() {
+        this.width = 19
+        this.height = 19
+        this.status = 'playing' // playing, black win, white win
+        this.grid = {}
+        this.turn = 'b'
+
+        for (let x = -4; x < this.width + 4; x++) {
+            this.grid[x] = {}
+            for (let y = -4; y < this.height + 4; y++) {
+                this.grid[x][y] = { color: '', score: 0 }
+            }
+        }
+    }
+
+    place(x, y) {
+        if (this.status !== 'playing') return
+        this.grid[x][y].color = this.turn
+        this.turn = this.turn === 'b' ? 'w' : 'b'
+        this.judge()
+    }
+
+    judge() {
+        for (let x = 0; x < this.width; x++) {
+            for (let y = 0; y < this.height; y++) {
+                this.checkLine(x, y, 1, 0)
+                this.checkLine(x, y, -1, 0)
+                this.checkLine(x, y, 0, 1)
+                this.checkLine(x, y, 0, -1)
+                this.checkLine(x, y, 1, 1)
+                this.checkLine(x, y, -1, -1)
+                this.checkLine(x, y, 1, -1)
+                this.checkLine(x, y, -1, 1)
+            }
+        }
+    }
+
+    checkLine(x, y, vx, vy) {
+        let s = ''
+        for (let i = 0; i < 5; i++) {
+            s += this.grid[x + i * vx][y + i * vy].color
+        }
+        if (s === 'bbbbb') this.status = 'black win'
+        if (s === 'wwwww') this.status = 'white win'
+    }
+}
+
+
